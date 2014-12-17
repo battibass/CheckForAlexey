5efbf59a3fe2cd0aec2c3b3da8b77ba2d21feb70
Carlo Battilana <carlo.battilana@gmail.com>
2014-12-17 18:55:27 +0100

    Added documentation

--- CondTools/TrkErrorMatricesDumper/test/printTrkErrorMatrices_cfg.py
+++ CondTools/TrkErrorMatricesDumper/test/printTrkErrorMatrices_cfg.py
@@ -2,10 +2,18 @@ import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("PrintTrkErrors")
 
+# CB : this runNumber is used to generate a dummy source with 1 empty 
+# event but allows to get the database in an IOV basis, one hat to
+# define run number on the basis of the IOVs listed with something like:
+# cmscond_list_iov --connect frontier://FrontierProd/CMS_COND_31X_ALIGNMENT --tag TrackerAlignmentErr_2009_v2_express
+# for ever run in since + 1 the run number has to be set and the comparison has to be run
+
 runNumber = 231615
 
 process.load("CondCore.DBCommon.CondDBSetup_cfi")
 
+# CB here is the DB source to get the OLD error records
+# the relevant tag has to be put there or a sqlite file could be used
 process.errorSource = cms.ESSource("PoolDBESSource",
                                    process.CondDBSetup,
                                    toGet = cms.VPSet(cms.PSet(record = cms.string('TrackerAlignmentErrorRcd'),
@@ -15,6 +23,8 @@ process.errorSource = cms.ESSource("PoolDBESSource",
                                    connect = cms.string('frontier://FrontierProd/CMS_COND_31X_ALIGNMENT')
                                    )
 
+# CB here is the DB source to get the NEW error records
+# the relevant tag has to be put there or a sqlite file could be used
 process.extendedErrorSource = cms.ESSource("PoolDBESSource",
                                    process.CondDBSetup,
                                    toGet = cms.VPSet(cms.PSet(record = cms.string('TrackerAlignmentErrorExtendedRcd'),
